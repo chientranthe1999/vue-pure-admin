16887349699b9f70ca832e3ce4978765fed0f4d9
xiaoxian521 <1923740402@qq.com>
2022-04-22 06:22:44 +0000

    ci: Automatic deployment from github actions

---
+++ assets/system.c1d0c915.js
@@ -0,0 +1,1 @@
+import{cq as e}from"./index.05764e95.js";const r=t=>e.request("post","/user",{data:t}),o=t=>e.request("post","/role",{data:t}),p=t=>e.request("post","/dept",{data:t});export{o as a,r as b,p as g};
